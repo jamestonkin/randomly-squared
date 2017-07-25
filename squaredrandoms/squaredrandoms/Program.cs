689487614cd72a3e4ae681f2918aa935132a00d0
James W. Tonkin <btonkin71@comcast.net>
2017-07-25 10:32:35 -0500

    Exercise complete.

--- squaredrandoms/squaredrandoms/Program.cs
+++ squaredrandoms/squaredrandoms/Program.cs
@@ -10,6 +10,40 @@ namespace squaredrandoms
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            List<int> randoList = new List<int>();
+            List<int> squaredList = new List<int>();
+            for(int i = 0; i < 20; i++)
+            {
+                randoList.Add(random.Next(1, 50));
+            }
+
+            foreach(int randomNumber in randoList)
+            {
+                Console.WriteLine(randomNumber);
+                int squareUp = randomNumber * randomNumber;
+                squaredList.Add(squareUp);
+            }
+
+            Console.WriteLine("Squared Even numbers");
+            Console.WriteLine("=====================");
+
+            foreach (int squaredNumber in squaredList.ToList())
+            {
+                if (squaredNumber % 2 != 0)
+                {
+                    squaredList.Remove(squaredNumber);
+                }
+                else
+                {
+
+                    Console.WriteLine(squaredNumber);
+
+                }
+            }
+
+            Console.Read();
+
         }
     }
 }
